2c200f350da1f0678ff9bb3806f4485dfcf78195
Em Jordan <sylvrs@proton.me>
2022-07-21 22:39:43 -0500

    Implement headers

--- src/libforms/buttons/image/ImageType.php
+++ src/libforms/buttons/image/ImageType.php
@@ -1,12 +1,28 @@
 <?php
+/**
+ *  _ _ _      __
+ * | (_) |    / _|
+ * | |_| |__ | |_ ___  _ __ _ __ ___  ___
+ * | | | '_ \|  _/ _ \| '__| '_ ` _ \/ __|
+ * | | | |_) | || (_) | |  | | | | |a\__ \
+ * |_|_|_.__/|_| \___/|_|  |_| |_| |_|___/
+ *
+ * This library is free software licensed under the MIT license.
+ * For more information about the license, visit the link below:
+ *
+ * https://opensource.org/licenses/MIT
+ *
+ * Copyright (c) 2022 Matthew Jordan
+ */
+declare(strict_types=1);
 
 namespace libforms\buttons\image;
 
 use pocketmine\utils\EnumTrait;
 
 /**
- * @method static ImageType PATH()
- * @method static ImageType URL()
+ * @method static self PATH()
+ * @method static self URL()
  */
 class ImageType {
 	use EnumTrait;
